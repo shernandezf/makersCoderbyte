15361d2c495edbd7f089c42e3482bd154be6a2a3
shernandezf <s.hernandezf@uniandes.edu.co>
2022-05-22 14:57:16 -0500

    api finalizado con filter y paginación

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from email import message
 import sqlite3
 from turtle import title
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, abort, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-
+import json
 import nasapy
 import os
 from datetime import datetime
@@ -71,33 +71,64 @@ class Photo(Resource):
         db.session.add(photo)
         db.session.commit()
         return photo
-    @marshal_with(resource_fields)
+    
     def get(self,photo_id):
         if photo_id==0:
-            ROWS_PER_PAGE=5
             args = request.args
+
             args.to_dict()
             args.get("filter", default="", type=str)
             args.get("filter_value", default="", type=str)
+            args.get("limit", default=5, type=int)
+            if  args.get("limit") is not None:
+                
+                ROWS_PER_PAGE=int(args.get("limit"))
+            else:
+                ROWS_PER_PAGE=PhotoModel.query.count()
             if args.get("filter") is not None or args.get("filter_value") is not None:
                 expresion='%'+str(args.get("filter_value"))+'%'
                 if args.get("filter")=='id':
-                    result=PhotoModel.query.filter(PhotoModel.id.ilike(expresion)).all()
+                    result=PhotoModel.query.filter(PhotoModel.id.ilike(expresion)).paginate(page=1, per_page=ROWS_PER_PAGE)
                 elif args.get("filter")=='explanation':
-                    result=PhotoModel.query.filter(PhotoModel.explanation.ilike(expresion)).all()
+                    result=PhotoModel.query.filter(PhotoModel.explanation.ilike(expresion)).paginate(page=1, per_page=ROWS_PER_PAGE)
                 elif args.get("filter")=='hdurl':
-                    result=PhotoModel.query.filter(PhotoModel.hdurl.ilike(expresion)).all()
+                    result=PhotoModel.query.filter(PhotoModel.hdurl.ilike(expresion)).paginate(page=1, per_page=ROWS_PER_PAGE)
                 elif args.get("filter")=='title':
-                    result=PhotoModel.query.filter(PhotoModel.title.ilike(expresion)).all()
+                    result=PhotoModel.query.filter(PhotoModel.title.ilike(expresion)).paginate(page=1, per_page=ROWS_PER_PAGE)
                 elif args.get("filter")=='url':
-                    result=PhotoModel.query.filter(PhotoModel.url.ilike(expresion)).all()
+                    result=PhotoModel.query.filter(PhotoModel.url.ilike(expresion)).paginate(page=1, per_page=ROWS_PER_PAGE)
             else:
-                result=PhotoModel.query.all()
+                result=PhotoModel.query.paginate(page=1, per_page=ROWS_PER_PAGE)
+            paginacion=result.items
+            resultado=[{
+                'id': photo.id,
+                'explanation': photo.explanation,
+                'hdurl': photo.hdurl,
+                'title': photo.title,
+                'url': photo.url
+            }for photo in paginacion]   
+            resultado_final=jsonify({
+            'success':True,
+            'results':resultado
+            })
         else:
-            result=PhotoModel.query.filter_by(id=photo_id).first()
-        if not result:
+            resultado=PhotoModel.query.filter_by(id=photo_id).first()
+            datos={
+                'id': resultado.id,
+                'explanation': resultado.explanation,
+                'hdurl': resultado.hdurl,
+                'title': resultado.title,
+                'url': resultado.url
+            }
+            resultado_final =jsonify({
+            'success':True,
+            'results':datos
+            })
+            
+        if not resultado_final:
             abort(404,message="La imagen no existe.")
-        return result
+        
+        return resultado_final
     @marshal_with(resource_fields)
     def put(self, photo_id,fecha):
         if photo_id==0:
